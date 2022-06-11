23b7070fea8907ac5bbd25b3ce9dbbd1b7faea5a
sunny <sunny2004.kim@gmail.com>
2022-06-11 15:37:17 -0500

    addingthings

--- src/App.js
+++ src/App.js
@@ -4,22 +4,52 @@ import Button from 'react-bootstrap/Button';
 import Deso from 'deso-protocol';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    // getInitialState
+    this.state = {
+      login: false
+    }
+  }
+
   login = async () => {
     const deso = new Deso();
     const request = 3;
     const response = await deso.identity.login(request);
+
+    this.setState({
+      login: true
+    })
   }
 
   render() {
-    return (
-      <div className="main">
-        <div className="headerbuttons">
-          <div className="tbutt"><Button variant="outline-secondary" onClick={this.login}>Login</Button></div>
-        </div>
+    if(this.state.login) {
+      return (
+        <div className="main">
+          <div className="headerbuttons">
+            <div className="tbutt"><Button variant="outline-secondary" onClick={this.login}>Login</Button></div>
+          </div>
 
-        
-      </div>
-    );
+          <div className="main">
+            {"pp"}
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div className="main">
+          <div className="headerbuttons">
+            <div className="tbutt"><Button variant="outline-secondary" onClick={this.login}>Login</Button></div>
+          </div>
+
+          <div className="main">
+            login dummy
+          </div>
+        </div>
+      );
+    }
+
   }
 }
 
