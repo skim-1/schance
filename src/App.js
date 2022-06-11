c0074e5f87abee54617439d995e5498e36219887
sunny <sunny2004.kim@gmail.com>
2022-06-11 13:41:51 -0500

    poo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import './styles/App.css';
+import Button from 'react-bootstrap/Button';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <div className="text">
+        {"Welcome to SecondChance"}
+
+        <Button variant="light">Get Started</Button>
+      </div>
+    );
+  }
 }
 
 export default App;
