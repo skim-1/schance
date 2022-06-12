8509b6b5208500fb3c4bef3b7700b0dffbe2a8c9
Damien <damien.z.hall@gmail.com>
2022-06-12 03:21:13 -0500

    better login looks :P

---
+++ codesnippetbackup.js
@@ -0,0 +1,13 @@
+<div className="container-fourth center">
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#ffffff" fill-opacity="1" d="M0,64L30,74.7C60,85,120,107,180,122.7C240,139,300,149,360,133.3C420,117,480,75,540,80C600,85,660,139,720,149.3C780,160,840,128,900,133.3C960,139,1020,181,1080,192C1140,203,1200,181,1260,165.3C1320,149,1380,139,1410,133.3L1440,128L1440,0L1410,0C1380,0,1320,0,1260,0C1200,0,1140,0,1080,0C1020,0,960,0,900,0C840,0,780,0,720,0C660,0,600,0,540,0C480,0,420,0,360,0C300,0,240,0,180,0C120,0,60,0,30,0L0,0Z"></path></svg>
+  <img src={titleLogo} style={{position: "absolute", top: "20px"}}></img>
+</div>
+<div className="container-fourth center">
+  <h4 className="white-text">Beleive in second chances? We do too.</h4>
+</div>
+<div className="container-fourth center">
+  <button className="big-button rounded blue white-text" onClick={this.login}>Login</button>
+</div>
+<div className="container-fourth center">
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 300"><path fill="#5000ca" fillOpacity="0.1" d="M0,0L26.7,5.3C53.3,11,107,21,160,64C213.3,107,267,181,320,224C373.3,267,427,277,480,240C533.3,203,587,117,640,112C693.3,107,747,181,800,197.3C853.3,213,907,171,960,165.3C1013.3,160,1067,192,1120,181.3C1173.3,171,1227,117,1280,85.3C1333.3,53,1387,43,1413,37.3L1440,32L1440,320L1413.3,320C1386.7,320,1333,320,1280,320C1226.7,320,1173,320,1120,320C1066.7,320,1013,320,960,320C906.7,320,853,320,800,320C746.7,320,693,320,640,320C586.7,320,533,320,480,320C426.7,320,373,320,320,320C266.7,320,213,320,160,320C106.7,320,53,320,27,320L0,320Z"></path></svg>
+</div>
