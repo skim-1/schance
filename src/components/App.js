19650a846f0a19eeef7bf2532eaeed1aa3281b9e
sunny <sunny2004.kim@gmail.com>
2022-06-12 12:32:21 -0500

    brody

--- src/components/App.js
+++ src/components/App.js
@@ -19,7 +19,7 @@ class App extends Component {
       chatlog: [],
       ulog: [],
       value: '',
-      currentUser: 'BC1YLgaCiCZ32rAxTAYLA8HamjsWV6nuzqBBN5aZMnRC3zMWU29cuTh', //adjust this to the other user
+      currentUser: '', //adjust this to the other user
       username: '', //string of your user's username
     }
 
--- src/components/Chatbox.js
+++ src/components/Chatbox.js
@@ -19,58 +19,62 @@ const Chatbox = props => {
     var totalScore = 0;
     var msgCount = 0;
 
-    return (
-    <div>
-          <div> {
+    if(props.currentUser !== '') {
+      return (
+      <div>
+            <div> {
+            props.msgs.map((item_, index_) => {
 
-          props.msgs.map((item_, index_) => {
+              props.usrs.map((item, index) => {
+                if (item.key === item_.SenderMessagingPublicKey) {
+                  otherName = item.username;
+                }
+              })
 
-            props.usrs.map((item, index) => {
-              if (item.key === item_.SenderMessagingPublicKey) {
-                otherName = item.username;
-              }
+              if (item_.SenderMessagingPublicKey === props.currentUser && item_.RecipientMessagingPublicKey === props.usrkey) {
+              var Sentiment = require('sentiment');
+              var sentiment = new Sentiment();
+              var result = sentiment.analyze(item_.DecryptedMessage);
+
+              totalScore += result.score;
+              msgCount ++;
+
+              var avg = totalScore/msgCount;
+              console.log(totalScore);
+              console.log(avg.toFixed(2)); // truncated number
+
+              return (<div key={index_}>
+                {
+
+                otherName + ": " + item_.DecryptedMessage
+                }<br/>
+              </div>);
+            } else if (item_.SenderMessagingPublicKey === props.usrkey && item_.RecipientMessagingPublicKey === props.currentUser) {
+              return (<div key={index_}>
+                {
+                props.username + ": " + item_.DecryptedMessage
+                }<br/></div>);
+                }
             })
-
-            if (item_.SenderMessagingPublicKey === props.currentUser && item_.RecipientMessagingPublicKey === props.usrkey) {
-            var Sentiment = require('sentiment');
-            var sentiment = new Sentiment();
-            var result = sentiment.analyze(item_.DecryptedMessage);
-
-            totalScore += result.score;
-            msgCount ++;
-
-            var avg = totalScore/msgCount;
-            console.log(totalScore);
-            console.log(avg.toFixed(2)); // truncated number
-
-            return (<div key={index_}>
-              {
-
-              otherName + ": " + item_.DecryptedMessage
-              }<br/>
-            </div>);
-          } else if (item_.SenderMessagingPublicKey === props.usrkey && item_.RecipientMessagingPublicKey === props.currentUser) {
-            return (<div key={index_}>
-              {
-              props.username + ": " + item_.DecryptedMessage
-              }<br/></div>);
-              }
-          })
-          }
-          </div>
-      <Form>
-                              <Form.Label>Message <input type="text" value={props.value} onChange={props.handleChange} /> </Form.Label>
+            }
+            </div>
+        <Form>
+                                <Form.Label>Message <input type="text" value={props.value} onChange={props.handleChange} /> </Form.Label>
 
 
-                            <Button variant="primary" onClick={() => props.sendMsg(
-                                  props.usrkey,
-                                  props.currentUser,
-                                  props.value)}>
-                              Send
-                            </Button>
-                          </Form>
-                          </div>
-    );
+                              <Button variant="primary" onClick={() => props.sendMsg(
+                                    props.usrkey,
+                                    props.currentUser,
+                                    props.value)}>
+                                Send
+                              </Button>
+                            </Form>
+                            </div>
+      );
+    } else { //this is what displays when no one is selected
+      return(<div>joe</div>)
+    }
+
 };
 
 export default Chatbox;
