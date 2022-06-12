84d077804f0faf8fffdfc5212817c78ca83c02cb
Damien <damien.z.hall@gmail.com>
2022-06-12 16:41:33 -0500

    chatbox update

--- src/components/Chatbox.js
+++ src/components/Chatbox.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import '../styles/App.css';
+import '../styles/Chatbox.css';
 import Button from 'react-bootstrap/Button';
 import Deso from 'deso-protocol';
 
@@ -21,55 +21,39 @@ const Chatbox = props => {
 
     return (
     <div>
-          <div> {
-
-          props.msgs.map((item_, index_) => {
-
-            props.usrs.map((item, index) => {
-              if (item.key === item_.SenderMessagingPublicKey) {
-                otherName = item.username;
-              }
-            })
-
-            if (item_.SenderMessagingPublicKey === props.currentUser && item_.RecipientMessagingPublicKey === props.usrkey) {
-            var Sentiment = require('sentiment');
-            var sentiment = new Sentiment();
-            var result = sentiment.analyze(item_.DecryptedMessage);
-
-            totalScore += result.score;
-            msgCount ++;
-
-            var avg = totalScore/msgCount;
-            console.log(totalScore);
-            console.log(avg.toFixed(2)); // truncated number
-
-            return (<div key={index_}>
-              {
-
-              otherName + ": " + item_.DecryptedMessage
-              }<br/>
-            </div>);
+      <div>{
+        props.msgs.map((item_, index_) => {
+          props.usrs.map((item, index) => {
+            if (item.key === item_.SenderMessagingPublicKey) {
+              otherName = item.username;
+            }
+          });
+          if (item_.SenderMessagingPublicKey === props.currentUser && item_.RecipientMessagingPublicKey === props.usrkey) {
+          var Sentiment = require('sentiment');
+          var sentiment = new Sentiment();
+          var result = sentiment.analyze(item_.DecryptedMessage);
+          totalScore += result.score;
+          msgCount ++;
+          var avg = totalScore/msgCount;
+          console.log(totalScore);
+          console.log(avg.toFixed(2));
+          return (<div key={index_}>{otherName + ": " + item_.DecryptedMessage}<br/></div>);
           } else if (item_.SenderMessagingPublicKey === props.usrkey && item_.RecipientMessagingPublicKey === props.currentUser) {
-            return (<div key={index_}>
-              {
-              props.username + ": " + item_.DecryptedMessage
-              }<br/></div>);
-              }
-          })
+            return (<div key={index_}>{props.username + ": " + item_.DecryptedMessage}<br/></div>);
           }
+        })}
           </div>
       <Form>
-                              <Form.Label>Message <input type="text" value={props.value} onChange={props.handleChange} /> </Form.Label>
-
-
-                            <Button variant="primary" onClick={() => props.sendMsg(
+        <Form.Label>Message <input type="text" value={props.value} onChange={props.handleChange} /> </Form.Label>
+          <Button variant="primary" onClick={() => props.sendMsg(
                                   props.usrkey,
                                   props.currentUser,
-                                  props.value)}>
-                              Send
-                            </Button>
-                          </Form>
-                          </div>
+                                  props.value)}
+          >
+            Send
+          </Button>
+        </Form>
+      </div>
     );
 };
 
